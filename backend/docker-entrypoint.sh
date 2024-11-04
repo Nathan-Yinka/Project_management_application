dc271f00e3ae37bbb3ec269dcca2d7a3cad39379
Nathan-Yinka <oludarenathaniel@gmail.com>
2024-11-04 05:43:54 +0100

    bug fixed

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -8,9 +8,6 @@ set -e
 echo "Running migrations..."
 python manage.py migrate
 
-# Collect static files
-echo "Collecting static files..."
-python manage.py collectstatic --noinput
 
 # Start the server
 echo "Starting server..."
